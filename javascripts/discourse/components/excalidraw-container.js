940eff84f4c288815d47179e20fb960bf21c4d40
jjaffeux <j.jaffeux@gmail.com>
2021-04-26 10:51:33 +0200

    initial commit

---
+++ javascripts/discourse/components/excalidraw-container.js
@@ -0,0 +1,156 @@
+import { action } from "@ember/object";
+import loadScript from "discourse/lib/load-script";
+import { ajax } from "discourse/lib/ajax";
+import Component from "@ember/component";
+import { schedule } from "@ember/runloop";
+
+export default Component.extend({
+  tagName: "",
+  layoutName: "components/excalidraw-container",
+  postModel: null,
+  isEditing: false,
+  isLoadingReact: false,
+  isSavingScene: false,
+  scene: null,
+  viewModeEnabled: true,
+  _ref: null,
+
+  didInsertElement() {
+    this._super(...arguments);
+
+    this.set("isLoadingReact", true);
+
+    loadScript(
+      "https://unpkg.com/react@17.0.2/umd/react.production.min.js"
+    ).then(() => {
+      return loadScript(
+        "https://unpkg.com/react-dom@17.0.2/umd/react-dom.production.min.js"
+      ).then(() => {
+        return loadScript(
+          "https://unpkg.com/@excalidraw/excalidraw@0.6.0/dist/excalidraw.production.min.js"
+        )
+          .then(() => {
+            this.set("_ref", React.createRef());
+
+            this._renderExcalidraw();
+          })
+          .finally(() => {
+            this.set("isLoadingReact", false);
+          });
+      });
+    });
+  },
+
+  _renderExcalidraw() {
+    schedule("afterRender", () => {
+      if (!this.postModel) {
+        return;
+      }
+
+      const excalidrawCanvasContainer = document.querySelector(
+        `article[data-post-id="${
+          this.postModel.id
+        }"] .excalidraw-canvas-container`
+      );
+
+      if (excalidrawCanvasContainer) {
+        const excalidrawComponent = React.createElement(Excalidraw.default, {
+          ref: this._ref,
+          initialData: JSON.parse(
+            this.scene
+              .replace(/[\u2018\u2019]/g, "'")
+              .replace(/[\u201C\u201D]/g, '"')
+          ),
+          isCollaborating: false,
+          viewModeEnabled: !this.isEditing,
+          UIOptions: {
+            canvasActions: {
+              saveScene: true,
+              saveAsScene: true
+            }
+          }
+        });
+
+        ReactDOM.render(excalidrawComponent, excalidrawCanvasContainer);
+      }
+    });
+  },
+
+  @action
+  onEdit() {
+    this.set("isEditing", true);
+
+    this._renderExcalidraw();
+  },
+
+  @action
+  onSave() {
+    this.set("isSavingScene", true);
+
+    return ajax(`/posts/${this.postModel.id}`, {
+      type: "GET",
+      cache: false
+    }).then(result => {
+      const newRaw = result.raw.replace(
+        /\[wrap=excalidraw\]\n(.*)\n\[\/wrap\]/gs,
+        (match, ignored, off) => {
+          const pick = (obj, ...args) => ({
+            ...args.reduce((res, key) => ({ ...res, [key]: obj[key] }), {})
+          });
+
+          return (
+            "[wrap=excalidraw]\n" +
+            JSON.stringify(
+              {
+                elements: this._ref.current.getSceneElements(),
+                appState: Object.assign(
+                  {},
+                  pick(
+                    this._ref.current.getAppState(),
+                    "theme",
+                    "viewBackgroundColor",
+                    "currentChartType",
+                    "currentItemBackgroundColor",
+                    "currentItemEndArrowhead",
+                    "currentItemFillStyle",
+                    "currentItemFontFamily",
+                    "currentItemFontSize",
+                    "currentItemLinearStrokeSharpness",
+                    "currentItemOpacity",
+                    "currentItemRoughness",
+                    "currentItemStartArrowhead",
+                    "currentItemStrokeColor",
+                    "currentItemStrokeSharpness",
+                    "currentItemStrokeStyle",
+                    "currentItemStrokeWidth",
+                    "currentItemTextAlign",
+                    "zoom",
+                    "offsetLeft",
+                    "offsetTop",
+                    "scrollX",
+                    "scrollY"
+                  ),
+                  { viewModeEnabled: true }
+                )
+              },
+              null,
+              2
+            ) +
+            "\n[/wrap]"
+          );
+        }
+      );
+
+      const save = this.postModel.save({
+        raw: newRaw,
+        edit_reason: I18n.t(themePrefix("excalidraw.edit_reason"))
+      });
+
+      if (save && save.then) {
+        save.finally(() => this.set("isSavingScene", false));
+      } else {
+        this.set("isSavingScene", false);
+      }
+    });
+  }
+});
---
+++ javascripts/discourse/initializers/embed-excalidraw-setup.js
@@ -0,0 +1,40 @@
+import { popupAjaxError } from "discourse/lib/ajax-error";
+import { withPluginApi } from "discourse/lib/plugin-api";
+
+export default {
+  name: "embed-excalidraw",
+
+  initialize() {
+    withPluginApi("0.11.1", api => {
+      api.decorateCookedElement(
+        (element, postDecorator) => {
+          const draw = element.querySelector(".d-wrap[data-wrap=excalidraw]");
+          if (!draw || !postDecorator) {
+            return;
+          }
+
+          const postModel = postDecorator.getModel();
+          if (!postModel) {
+            return;
+          }
+
+          const scene = draw.innerText.trim();
+
+          draw.innerText = "";
+
+          const component = api.container.owner
+            .factoryFor("component:excalidraw-container")
+            .create({
+              scene,
+              postModel
+            });
+          component.renderer.appendTo(component, draw);
+        },
+        {
+          onlyStream: true,
+          id: "embed-excalidraw"
+        }
+      );
+    });
+  }
+};
